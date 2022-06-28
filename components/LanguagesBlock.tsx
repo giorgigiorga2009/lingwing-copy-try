521d5bfbac64d51558f6130ce1b7cc5a381f76aa
Kate Astrid <kateastrid@Kates-MacBook-Pro.local>
2022-06-28 17:43:24 +0200

    add first language tile

--- components/LanguagesBlock.tsx
+++ components/LanguagesBlock.tsx
@@ -1,30 +1,20 @@
-import classNames from "classnames";
-import { FC } from "react";
-import { SwitchedLanguages } from "./languages";
+import classNames from 'classnames'
+import { FC } from 'react'
+import { SwitchedLanguages } from './languages'
 import style from './LanguagesBlock.module.scss'
 
 interface Props {
-    language: SwitchedLanguages
+  language: SwitchedLanguages
 }
 
-export const LanguageTile: FC<Props> = ({language}) => {
+export const LanguageTile: FC<Props> = ({ language }) => {
   return (
     <div className={classNames(style.tileContainer, style[language.short])}>
-        {/* routing by onclick */}
-        <span className={style.start}>Start</span>
-            <span className={style.title}>{language.long}</span>
-            <span className={classNames(style.languageFlag, style[language.short])}/>
-            <span className={classNames(style.parrot, style[language.short])} />
-
-            
-
-            {/* {/* <span class="lang-o" style="background-image:url('../themes/images/v2/eng_o.jpg')">
-              <span class="lang-parrot">
-                <img src="../themes/images/v2/eng_parrot.png">
-              </span>
-            </span>  */}
-
-
-      </div>
-    )
+      {/* routing by onclick */}
+      <span className={style.start}>Start</span>
+      <span className={style.title}>{language.long}</span>
+      <span className={classNames(style.languageFlag, style[language.short])} />
+      <span className={classNames(style.parrot, style[language.short])} />
+    </div>
+  )
 }
