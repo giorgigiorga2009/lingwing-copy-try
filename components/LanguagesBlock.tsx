67bc09cb02a5747aa69e99b15bdd4c763788824b
Kate Astrid <kateastrid@Kates-MacBook-Pro.local>
2022-06-28 16:25:03 +0200

    add title icon

--- components/LanguagesBlock.tsx
+++ components/LanguagesBlock.tsx
@@ -10,22 +10,20 @@ interface Props {
 export const LanguageTile: FC<Props> = ({language}) => {
   return (
     <div className={classNames(style.tileContainer, style[language.short])}>
-
-        <a ng-href="en/wizard/eng" href="en/wizard/eng">
-
-            <span class="lang-p" style="background-image:url('../themes/images/v2/eng_p.png')">
-              <span class="lang-name ng-binding">English</span>
+        {/* routing by onclick */}
+            <span className={classNames(style.languageImage, style[language.short])}>
+              <span className={style.title}>{language.long}</span>
             </span>
 
-            <span class="lang-o" style="background-image:url('../themes/images/v2/eng_o.jpg')">
+            {/* <span class="lang-o" style="background-image:url('../themes/images/v2/eng_o.jpg')">
               <span class="lang-start ng-binding">Start</span>
               <span class="lang-parrot">
                 <img src="../themes/images/v2/eng_parrot.png">
               </span>
             </span>
 
-            <img class="lang-flag" src="../themes/images/v2/eng_flag.png">
-          </a>
+            <img class="lang-flag" src="../themes/images/v2/eng_flag.png"> */}
+
       </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import { Header } from '../components/Header'
+import { Language } from '../components/languages'
+import { LanguageTile } from '../components/LanguagesBlock'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
       <div className={styles.container}>
         <Header />
+        <LanguageTile language={{long: 'English', short: 'eng'} as Language} />
       </div>
   )
 }
