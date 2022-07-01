2ee6ef1890fd81045e500db1027bbeb9772d1c26
Kate Astrid <kateastrid@Kates-MacBook-Pro.local>
2022-07-01 19:03:19 +0200

    small editing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <div>
       <Head>
         <link
           rel="icon"
@@ -15,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
-    </Layout>
+    </div>
   )
 }
 
