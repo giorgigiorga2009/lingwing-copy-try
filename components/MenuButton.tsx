3f7b39ed7eaa3b7f4825c4a76cd4495d19eb5320
Kate Astrid <kateastrid@Kates-MacBook-Pro.local>
2022-07-05 15:51:58 +0200

    css editing

---
+++ components/MenuButton.tsx
@@ -0,0 +1,53 @@
+import { FC, useState } from 'react'
+import styles from './MenuButton.module.scss'
+import classnames from 'classnames'
+
+const COURSES = ["English", "Spanish", "Georgian", "Russian", "Turkish", "Bengali"]
+const ABOUT_COMPANY = ["Why with Us", "Certificate", "Partners", "Blog", "Jobs", "License Agreement", "Privacy Policy"]
+const PREMIUM = ["Entrant", "Prices", "Buy a gift", "Coupon activation"]
+const HELP = ["FAQ", "Contact us"]
+
+interface SectionProps {
+  options: string[]
+  title: string
+}
+
+const Section: FC<SectionProps> = ({ options, title }) => {
+  return (
+    <section>
+      <h1>{title}</h1>
+      <div className={styles.list}>
+        {options.map(element => (
+          <span key={element}>{element}</span>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+const MenuButton: FC = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div className={styles.menuContainer}>
+      <div
+        className={classnames(styles.button, open ? styles.cross : styles.lines)}
+        onClick={() => setOpen(!open)} />
+      <a className={styles.logo} href="https://lingwing.com/en/"/>
+      {open && (
+        <div className={styles.menuContent}>
+          <div className={styles.column}>
+            <Section title="Courses" options={COURSES} />
+            <Section title="Premium" options={PREMIUM} />
+          </div>
+          <div className={styles.column}>
+            <Section title="Company" options={ABOUT_COMPANY} />
+            <Section title="Help" options={HELP} />
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MenuButton
