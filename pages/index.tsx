d54bbfcba92dd33c29e67b774687d47be2f1cbc4
KaitenZx <darkkosoy@gmail.com>
2022-07-07 04:46:25 +0400

    fixes, wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from 'next'
 import { Header } from '../components/header/Header'
-<<<<<<< HEAD
-import { LanguagesBlock} from '../components/LanguagesBlock'
-import styles from '../styles/Home.module.scss'
-=======
 import { LanguagesBlock } from '../components/LanguagesBlock'
 import { NetworkButtons } from '../components/NetworkButtons'
 import styles from '../styles/Home.module.css'
->>>>>>> 3a2d5b8 (fixes)
 
 const Home: NextPage = () => {
   return (
