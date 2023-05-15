4630a743ca369a712332d386721e404f82e61533
floyd910 <lgelava7@gmail.com>
2023-05-15 23:13:44 +0400

    promo-slider-started

--- components/dashboard/PromoSlider.tsx
+++ components/dashboard/PromoSlider.tsx
@@ -1,8 +1,25 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import ReactDOM from 'react-dom'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
 import style from './PromoSlider.module.scss'
 
 const PromoSlider: FC = () => {
-  return <div className={style.promo_carousel}></div>
+  return (
+    <div className={style.promo_slider}>
+      <Carousel>
+        <div>
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  )
 }
 
 export default PromoSlider
