c4ed6d96683e9e8b484141dd5b862290f37753e9
floyd910 <lgelava7@gmail.com>
2023-05-16 22:31:41 +0400

    promo_slider

--- components/dashboard/PromoSlider.tsx
+++ components/dashboard/PromoSlider.tsx
@@ -10,22 +10,31 @@ const PromoSlider: FC = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0)
   const slides = [
     {
-      background: '#f28e13',
-      title: 'Get the 10 Lari homeworks free!',
+      background: 'orange',
+      title: (
+        <h6>
+          Get the <span>10 Lari</span> homeworks free!
+        </h6>
+      ),
       button: 'Get Tasks',
     },
     {
-      background: '#f28e13',
-      title: 'Get 10 Lari homeworks free!',
+      background: 'orange',
+      title: (
+        <h6>
+          Learn <span>5 times</span> faster!
+        </h6>
+      ),
       button: 'Become a premium',
     },
 
     {
-      background: 'rgb(16, 24, 32)',
-      title: 'A joint project of Liberty and Lingwing',
+      background: 'dark',
+      title: <h6>A joint project of Liberty and Lingwing</h6>,
       button: 'Learn Georgian',
     },
   ]
+
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveIndex(prevIndex =>
@@ -36,34 +45,30 @@ const PromoSlider: FC = () => {
   }, [slides.length])
 
   return (
-    <div
-      className={style.promo_slider}
-      style={{ position: 'relative', maxWidth: '800px', margin: '0 auto' }}
-    >
+    <div className={style.promo_slider}>
       {slides.map((slide, index) => (
         <div
           className={classNames(
             style.slide,
             index === activeIndex ? style.slide_active : '',
+            index === 2 ? style.slide_dark : style.slide_orange,
           )}
-          key={index}
-          // style={{
-          //   opacity: index === activeIndex ? 1 : 0,
-          //   transition: `opacity 1.5s${
-          //     index === activeIndex ? '' : ', visibility 0s linear 1.5s'
-          //   }`,
-          //   visibility: index === activeIndex ? 'visible' : 'hidden',
-          //   position: 'absolute',
-          //   top: 0,
-          //   left: 0,
-          //   right: 0,
-          //   bottom: 0,
-          //   margin: 'auto',
-          //   maxHeight: '400px',
-          //   maxWidth: '100%',
-          // }}
-          style={{ background: `${slide.background}` }}
-        ></div>
+          key={slide.button}
+        >
+          {slide.title}
+          <button>
+            {index == 1 ? (
+              <img
+                src={'/themes/images/v2/banner-link-star.png'}
+                alt="Premium star icon"
+              />
+            ) : null}
+            {slide.button}
+          </button>
+          {index !== 2 ? <div className={style.right}></div> : null}
+          {index === 1 ? <div className={style.left}></div> : null}
+          {index === 0 ? <div className={style.left_get_tasks}></div> : null}
+        </div>
       ))}
     </div>
   )
