258a1aff9295ae5f73b537a1f813279955f1571d
floyd910 <lgelava7@gmail.com>
2023-05-18 08:38:25 -0700

    cleaned-code-in-promo-carousel

--- components/dashboard/MyMainCourse.tsx
+++ components/dashboard/MyMainCourse.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react'
 import { useTranslation } from '../../utils/useTranslation'
-import { getNumberWithComa } from '../../utils/getNumberWithComa'
 import FlagIcon from './FlagIcon'
 
 import style from './MyMainCourse.module.scss'
@@ -24,27 +23,27 @@ const MyMainCourse: FC<Props> = ({ course, myLanguage, LANGUAGE_NAMES }) => {
   const { t } = useTranslation()
 
   return (
-    <div className={style.wrapper}>
-      <div className={style.left_line_box}>
+    <div className={style.my_main_course}>
+      <div className={style.title_and_icon}>
         <FlagIcon
           item={myLanguage}
           size="big"
           LANGUAGE_NAMES={LANGUAGE_NAMES}
         />
         <h2 className={style.title}>
-          <span>{t(LANGUAGE_NAMES[myLanguage.nameCode])}</span>{' '}
+          <span>{t(LANGUAGE_NAMES[myLanguage.nameCode])}</span>
           <span>{course.name}</span>
         </h2>
       </div>
-      <div className={style.right_line_box}>
-        <div className={style.total_students}>
+      <div className={style.students_info}>
+        {/* <div className={style.total_students}>
           <span>{getNumberWithComa(course.uniqueStudentsCount)}</span>
           <span>{t('wizardStudents')}</span>
-        </div>
-        <div className={style.background}>
-          <div className={style.standart_icon_transparent}>
-            <div className={style.standart_icon_bg}>
-              <div className={style.icon_bg}></div>
+        </div> */}
+        <div className={style.icons_in_background}>
+          <div className={style.icon_sun_transparent}>
+            <div className={style.icon_sun}>
+              <div className={style.icon_fly}></div>
             </div>
           </div>
         </div>
