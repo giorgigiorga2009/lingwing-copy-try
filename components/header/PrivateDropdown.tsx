081b4f6b1d88e980719ecb4a59217c0f6e8b921d
floyd910 <lgelava7@gmail.com>
2023-05-25 01:06:48 +0400

    user-dropdown-started

--- components/header/PrivateDropdown.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-
-interface Props {}
-
-const PrivateDropdown: FC<Props> = () => {
-  return <div className="private_dropdown"></div>
-}
-
-export default PrivateDropdown
