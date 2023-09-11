c8338ed2a404a228483d4c09a61a5823ea49e375
Ilia Ghviniashvili <ili.ghviniashvili@gmail.com>
2023-09-11 14:37:39 +0400

    remove queryClientProvider from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,7 @@ import type { AppProps } from 'next/app'
 import { IntlProvider } from 'react-intl'
 import { Locale, messages } from '@utils/localization'
 import { SessionProvider } from 'next-auth/react'
-import { QueryClient, QueryClientProvider } from 'react-query'
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  const queryClient = new QueryClient()
   const { locale: initialLocale } = useRouter()
   const locale = initialLocale ? initialLocale : 'en'
 
@@ -29,9 +27,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         })(document);`}
       </Script>
       <SessionProvider session={session}>
-        <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
-        </QueryClientProvider>
       </SessionProvider>
     </IntlProvider>
   )
