d4c26dede5d4f8bccd862b81a4c55a24ef12d0c7
DQDKFA <66925932+Dmitriev365@users.noreply.github.com>
2023-11-14 14:48:20 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     defaultLocale: 'en',
     locales: ['en', 'bn', 'es', 'ka', 'ru', 'tr'],
   },
-  env: {
+    publicRuntimeConfig: {
+    // Your runtime environment variables here
     DEFAULT_URL: process.env.DEFAULT_URL,
     audioURL: process.env.AUDIO_URL,
     
