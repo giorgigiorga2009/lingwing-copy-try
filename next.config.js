2f82ecb8dd81d348bd6c105f835c9ef107d6e3b9
DQDKFA <66925932+Dmitriev365@users.noreply.github.com>
2023-11-14 17:27:38 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+const { parsed: taskDefinitionEnv } = process.env;
+
+module.exports = {
   reactStrictMode: true,
   i18n: {
     defaultLocale: 'en',
@@ -7,10 +10,14 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
     // runtime environment variables here
-
-    DEFAULT_URL: process.env.DEFAULT_URL,
-    audioURL: process.env.AUDIO_URL,
-
+    DEFAULT_URL: taskDefinitionEnv.DEFAULT_URL || process.env.DEFAULT_URL,
+    AUDIO_URL: taskDefinitionEnv.AUDIO_URL || process.env.AUDIO_URL,
+    FACEBOOK_ID: taskDefinitionEnv.FACEBOOK_ID || process.env.FACEBOOK_ID,
+    FACEBOOK_SECRET: taskDefinitionEnv.FACEBOOK_SECRET || process.env.FACEBOOK_SECRET,
+    GOOGLE_ID: taskDefinitionEnv.GOOGLE_ID || process.env.GOOGLE_ID,
+    GOOGLE_SECRET: taskDefinitionEnv.GOOGLE_SECRET || process.env.GOOGLE_SECRET,
+    NEXT_PUBLIC_SLACK_WEBHOOK_URL: taskDefinitionEnv.NEXT_PUBLIC_SLACK_WEBHOOK_URL || process.env.NEXT_PUBLIC_SLACK_WEBHOOK_URL,
+    SECRET: taskDefinitionEnv.SECRET || process.env.SECRET,
   },
   images: {
     domains: [
@@ -19,6 +26,4 @@ const nextConfig = {
       'lh3.googleusercontent.com',
     ],
   },
-}
-
-module.exports = nextConfig
+};
