464b33a0b2f0a145d435177a060919aa0224a314
LingwingGit <vato@lingwing.com>
2023-11-20 19:51:12 +0400

    changed only userCount with baseURl

--- utils/getUsersAmount.ts
+++ utils/getUsersAmount.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
-import getConfig from 'next/config'
-//const { process.env } = getConfig()
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_DEFAULT_URL || process.env.DEFAULT_URL,
+})
 
 export const getUsersAmount = () => {
-  return axios
-    .get(`${process.env.NEXT_PUBLIC_DEFAULT_URL ||process.env.DEFAULT_URL}/public/users/count`)
+  return instance
+    .get('/public/users/count')
     .then(response => response.data.data)
     .catch(error => console.log(error))
 }
